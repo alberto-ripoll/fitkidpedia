166bfefa02980e74734002380fcf885889ce359e
Alberto <alberto.garcia@zataca.com>
2024-04-13 17:13:20 +0200

    wip

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Src\Shared\Core\ValueObject\Uuid\UuidValue;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,16 +14,16 @@ class DatabaseSeeder extends Seeder
         DB::beginTransaction();
 
         $data = [
-            ['Lateral sin manos', '/ejercicios/lateral-sin-manos-quieta.png', '/ejercicios/lateral-sin-manos.MOV', '...', 'C', 'acrobacias'],
-            ['Paloma sin manos', '/ejercicios/paloma-sin-manos-quieta.png', '/ejercicios/paloma-sin-manos.MOV', '...', 'C', 'acrobacias'],
-            ['Flic flac', '/ejercicios/flic.png', '/ejercicios/flic.MOV', '...', 'C', 'acrobacias'],
-            ['Mortal carpado sin manos', '/ejercicios/paloma-sin-manos-quieta.png', '/ejercicios/carpado.MOV', '...', 'C', 'acrobacias'],
-            ['Infernal', '/ejercicios/infernal.jpeg', '/ejercicios/infernal.mp4', '...', 'H', 'fuerza'],
+            [UuidValue::create()->uuid(), 'Lateral sin manos', '/ejercicios/lateral-sin-manos-quieta.png', '/ejercicios/lateral-sin-manos.MOV', '...', 'C', 'acrobacias'],
+            [UuidValue::create()->uuid(), 'Paloma sin manos', '/ejercicios/paloma-sin-manos-quieta.png', '/ejercicios/paloma-sin-manos.MOV', '...', 'C', 'acrobacias'],
+            [UuidValue::create()->uuid(), 'Flic flac', '/ejercicios/flic.png', '/ejercicios/flic.MOV', '...', 'C', 'acrobacias'],
+            [UuidValue::create()->uuid(), 'Mortal carpado sin manos', '/ejercicios/paloma-sin-manos-quieta.png', '/ejercicios/carpado.MOV', '...', 'C', 'acrobacias'],
+            [UuidValue::create()->uuid(), 'Infernal', '/ejercicios/infernal.jpeg', '/ejercicios/infernal.mp4', '...', 'H', 'fuerza'],
         ];
 
         foreach ($data as $row) {
             DB::insert(
-                'insert into ejercicios (nombre, image, video, descripcion, dificultad, tipo) values (?, ?, ?, ?, ?, ?)',
+                'insert into ejercicios (id, nombre, image, video, descripcion, dificultad, tipo) values (?, ?, ?, ?, ?, ?, ?)',
                 $row
             );
         }
